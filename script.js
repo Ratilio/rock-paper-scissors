2fb92b3070f2bb0590f132242956e824722aceaa
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-04-16 17:09:15 -0600

    Corrected some errors in the messages. Started the game function

--- script.js
+++ script.js
@@ -10,18 +10,29 @@ function getComputerChoice(numberOptions){
 
   return computerWeapon;
 }
-console.log(getComputerChoice(numberOptions))
 
-let playerSelection = toUpperCase(prompt("Choose your weapon: rock, paper or scissors"));
+//let playerSelection = toUpperCase(prompt("Choose your weapon: rock, paper or scissors"));
+const playerSelection = 'ROCK';
 let computerSelection = getComputerChoice(numberOptions);
 
-function round(){
-  if (playerSelection === 'ROCK' && computerSelection === 'PAPER') console.log(`You loose. ${computerSelection} hugs ${playerSelection}`);
-  if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') console.log(`You win. ${computerSelection} obliterates ${playerSelection}`);
-  if (playerSelection === 'PAPER' && computerSelection === 'ROCK') console.log(`You win. ${computerSelection} hugs ${playerSelection}`);
-  if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') console.log(`You loose. ${computerSelection} slashes ${playerSelection}`);
-  if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') console.log(`You loose. ${computerSelection} obliterates ${playerSelection}`);
-  if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') console.log(`You win. ${computerSelection} slashes ${playerSelection}`);
-  if (playerSelection === computerSelection) console.log(`Tie for now.`);
+function playRound(playerSelection, computerSelection){
+  if (playerSelection === 'ROCK' && computerSelection === 'PAPER') return `You loose. ${computerSelection} hugs ${playerSelection}`;
+  if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') return `You win. ${playerSelection} obliterates ${computerSelection}`;
+  if (playerSelection === 'PAPER' && computerSelection === 'ROCK') return `You win. ${playerSelection} hugs ${computerSelection}`;
+  if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') return `You loose. ${computerSelection} slashes ${playerSelection}`;
+  if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') return `You loose. ${computerSelection} obliterates ${playerSelection}`;
+  if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') return `You win. ${playerSelection} slashes ${computerSelection}`;
+  if (playerSelection === computerSelection) return `Tie for now.`;
 
+}
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let index = 0; index < 5; index++) {
+    
+    
+  }
 }
