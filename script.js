0aa0278eb1b01cfc715df0705d3c3f7f803de9b5
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-04-16 17:49:30 -0600

    CReated game function and made a loop for 5 rounds with score.
    Created chooseWeapon function to every round ask for a new weapon and change computers weapon.
    
    Finished the game

--- script.js
+++ script.js
@@ -2,37 +2,54 @@ const numberOptions = 3;
 function getComputerChoice(numberOptions){
   //assigns random value to computer, from the only three options
   let randomNumber = Math.floor(Math.random() * numberOptions);//creates random number, only gives us 3 options (0,1,2)
-  let computerWeapon;
+  //let computerWeapon;
 
-  if (randomNumber === 0) computerWeapon = 'ROCK';
-  if (randomNumber === 1) computerWeapon = 'PAPER';
-  if (randomNumber === 2) computerWeapon = 'SCISSORS';
-
-  return computerWeapon;
+  if (randomNumber === 0) return 'ROCK';
+  if (randomNumber === 1) return 'PAPER';
+  if (randomNumber === 2) return 'SCISSORS';
 }
 
-//let playerSelection = toUpperCase(prompt("Choose your weapon: rock, paper or scissors"));
-const playerSelection = 'ROCK';
-let computerSelection = getComputerChoice(numberOptions);
+let playerSelection;
+let computerSelection;
+function chooseWeapon(){
+  playerSelection = prompt("Choose your weapon: rock, paper or scissors");
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = getComputerChoice(numberOptions);
+
+}
+
 
 function playRound(playerSelection, computerSelection){
-  if (playerSelection === 'ROCK' && computerSelection === 'PAPER') return `You loose. ${computerSelection} hugs ${playerSelection}`;
-  if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') return `You win. ${playerSelection} obliterates ${computerSelection}`;
-  if (playerSelection === 'PAPER' && computerSelection === 'ROCK') return `You win. ${playerSelection} hugs ${computerSelection}`;
-  if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') return `You loose. ${computerSelection} slashes ${playerSelection}`;
-  if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') return `You loose. ${computerSelection} obliterates ${playerSelection}`;
-  if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') return `You win. ${playerSelection} slashes ${computerSelection}`;
-  if (playerSelection === computerSelection) return `Tie for now.`;
+  if (playerSelection === 'ROCK' && computerSelection === 'PAPER') return 1;
+  if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') return 2;
+  if (playerSelection === 'PAPER' && computerSelection === 'ROCK') return 2;
+  if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') return 1;
+  if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') return 1;
+  if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') return 2;
+  if (playerSelection === computerSelection) return 0;
 
 }
-console.log(playRound(playerSelection, computerSelection));
+//console.log(playRound(playerSelection, computerSelection));
 
 function game(){
   let playerScore = 0;
   let computerScore = 0;
+  let score = 0;
 
   for (let index = 0; index < 5; index++) {
-    
-    
+    chooseWeapon();
+    score = playRound(playerSelection,computerSelection);
+    if (score == 2) playerScore++;
+    if (score == 1) computerScore++;
+    console.log(`Player: ${playerScore}.  Computer: ${computerScore}`);
+    console.log(`Computer weapon: ${computerSelection}`);
+    console.log(`Player weapon: ${playerSelection}`);
+
+
   }
+
+  if (playerScore === computerScore) console.log('Nobody wins');
+  if (playerScore < computerScore) console.log('Computer wins');
+  if (playerScore > computerScore) console.log('Player wins');
 }
+game();
